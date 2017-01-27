859e7c116142773fcb608f05d4540a6ac30516bd
Alexey <dominich.kh.cr@gmail.com>
2017-01-27 17:15:19 +0200

    create db filder if it not exists

--- server/utils/lowdb.js
+++ server/utils/lowdb.js
@@ -6,10 +6,13 @@ const fileAsync = require('lowdb/lib/file-async');
 
 
 
-const dbFileLocation = path.normalize(__dirname + './../db/db.json');
+const dbFolder = path.normalize(__dirname + './../db');
+const dbFileLocation = path.normalize(dbFolder + '/db.json');
 
 // check for db.json file
+if (!fs.existsSync(dbFolder)) fs.mkdirSync(dbFolder);
 if (!fs.existsSync(dbFileLocation)) fs.closeSync(fs.openSync(dbFileLocation, 'w'));
+
 
 
 const db = low(dbFileLocation, {
